7f147ffd339551e277b6d9c5cebb0ca9a78ab119
Roman <canvas1992@gmail.com>
2019-07-15 11:51:16 +0300

    cache

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import geoTimeGetter from 'geo-time-getter';
 
 import { GOOGLE_API_KEY } from './config'; 
+const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 const gtg = new geoTimeGetter({
-    GOOGLE: { apiKey: GOOGLE_API_KEY }
+    GOOGLE: { apiKey: GOOGLE_API_KEY },
+    cache: {
+        strategy: 'cacheFirst'
+    }
 });
 
 const test = async () => {
-    const result = await gtg.get('Novosibirsk');
-    console.log(result);
+    const result1 = await gtg.get('Novosibirsk');
+    console.log('result1', result1);
+    await sleep(3000);
+    const result2 = await gtg.get('Novosibirsk');
+    console.log('result2', result2);
 }
 
 test();
